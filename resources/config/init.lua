b65dc42c9dec9643c870700de6065a2b0b7176e1
Vernon Yang <vernon2gm@gmail.com>
2023-09-24 20:42:02 +0800

    Add nvim config for colorscheme

--- resources/config/init.lua
+++ resources/config/init.lua
@@ -1,16 +1,19 @@
 -- base setting
 vim.o.number = true
 vim.o.mouse = ""
-vim.cmd([[autocmd BufWritePre * %s/\s\+$//e]])
+vim.wo.colorcolumn = "80"
+vim.cmd [[ autocmd BufWritePre * %s/\s\+$//e ]]
 
 -- plugins setting
 vim.diagnostic.disable()
-require'lspconfig'.clangd.setup{}
+require'lspconfig'.clangd.setup { }
+vim.cmd [[ set background=dark ]]
+vim.cmd [[ colorscheme sonokai ]]
 
 -- plugins install
 vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use 'neovim/nvim-lspconfig'
+	use 'sainnhe/sonokai'
 end)
