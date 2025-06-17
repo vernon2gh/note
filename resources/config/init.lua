a94903052d29223a0412c9ca5f30d3cdc1bf39bd
Vernon Yang <vernon2gm@gmail.com>
2025-06-17 23:12:56 +0800

    Add dependencies for render-markdown.nvim
    Signed-off-by: Vernon Yang <vernon2gm@gmail.com>

--- resources/config/init.lua
+++ resources/config/init.lua
@@ -33,6 +33,10 @@ require("lazy").setup({
 	},
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"echasnovski/mini.nvim"
+		},
 		ft = {
 			"markdown",
 			"codecompanion"
