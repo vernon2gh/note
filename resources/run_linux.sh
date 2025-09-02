fbe0301cd46909969eebb462465cd467c0eab764
Vernon Yang <vernon2gm@gmail.com>
2025-09-02 14:26:12 +0800

    change default run_linux for x86_64
    Signed-off-by: Vernon Yang <vernon2gm@gmail.com>

--- resources/run_linux.sh
+++ resources/run_linux.sh
@@ -27,16 +27,16 @@ if [ $ARCH = "x86_64" ]; then
 	#	-append "root=/dev/sda rw console=ttyS0"	\
 	#	-enable-kvm -nographic $EXTRA
 
-	qemu-system-x86_64 -hda make_rootfs/out/ubuntu.qcow2	\
-		-kernel $DIR/arch/x86/boot/bzImage		\
-		-append "root=/dev/sda1 rw console=ttyS0"	\
-		-enable-kvm -nographic $EXTRA
-
-	#qemu-system-x86_64 -hda make_rootfs/out/fedora.qcow2			\
-	#	-kernel $DIR/arch/x86/boot/bzImage				\
-	#	-initrd make_rootfs/out/fedora-initramfs.img			\
-	#	-append "root=/dev/mapper/systemVG-LVRoot rw console=ttyS0"	\
+	#qemu-system-x86_64 -hda make_rootfs/out/ubuntu.qcow2	\
+	#	-kernel $DIR/arch/x86/boot/bzImage		\
+	#	-append "root=/dev/sda1 rw console=ttyS0"	\
 	#	-enable-kvm -nographic $EXTRA
+
+	qemu-system-x86_64 -hda make_rootfs/out/fedora.qcow2			\
+		-kernel $DIR/arch/x86/boot/bzImage				\
+		-initrd make_rootfs/out/fedora-initramfs.img			\
+		-append "root=/dev/mapper/systemVG-LVRoot rw console=ttyS0"	\
+		-enable-kvm -nographic $EXTRA
 fi
 
 if [ $ARCH = "arm64" ]; then
